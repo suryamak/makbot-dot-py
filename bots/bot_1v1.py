96b54f3daf8eeb0b5ed2e03df94cc56d68a32f6e
Surya Makthal <smaktha@ncsu.edu>
2019-09-08 01:41:13 -0400

    Refactoring, bot can store moves, types, and nicknames as battles progress

--- bots/bot_1v1.py
+++ bots/bot_1v1.py
@@ -9,18 +9,50 @@ import asyncio
 import re
 
 
-class Pokemon():
-    def __init__(self, name='', type=[], gender='', moves=[], item=''):
+class Room():
+    def __init__(self, room_id='', player_id='', mega=False):
+        self.id = room_id
+        self.primary_player = player_id
+        self.mega = mega
+    def print(self):
+        output = 'ID: ' + self.id + '\nPrimary Player: ' + self.primary_player + '\nMega Evolution Possible: ' + str(self.mega) + '\n'
+        return output
+
+class Player():
+    def __init__(self, room='', id='', name=''):
+        self.room = room
+        self.id = id
         self.name = name
-        self.type = type
+    def print(self):
+        output = 'Room: ' + self.room + '\nID: ' + self.id + '\nName: ' + self.name + '\n'
+        return output
+
+class Pokemon():
+    def __init__(self, room='', player='', name='', species='', types=[], gender='', moves=[], item=''):
+        self.room = room
+        self.player = player
+        self.name = name
+        self.species = species
+        self.types = types
         self.gender = gender
         self.moves = moves
         self.item = item
+    def print(self):
+        t = ''
+        m = ''
+        if len(self.types) > 0:
+            t = ', '.join(self.types)
+        if len(self.moves) > 0:
+            m = ', '.join(self.moves)
+        output = 'Room: ' + self.room + '\nPlayer: ' + self.player + '\nName: ' + self.name + '\nSpecies: ' + self.species + '\nTypes: ' + t + '\nGender: ' + self.gender + '\nMoves: ' + m + '\nItem: ' + self.item + '\n'
+        return output
 
 class ChallengeClient(showdown.Client):
 
-    players = {}
-    mega = False
+    battle_rooms = []
+    players = []
+    pokemon = []
+
 
     async def on_private_message(self, pm):
         if pm.recipient == self:
@@ -36,56 +68,102 @@ class ChallengeClient(showdown.Client):
 
     async def on_room_init(self, room_obj):
         if room_obj.id.startswith('battle-'):
-            await asyncio.sleep(3)
-            await room_obj.say('hi im the makbot')
-            await asyncio.sleep(1)
-            await room_obj.say("glhf")
-            await asyncio.sleep(1)
-            for player in self.players:
-                if self.players[player]['name'] != 'makbotdotpy':
-                    for poke in self.players[player]['pokemon']:
-                        await room_obj.say("!dt " + poke['name'])
-                        await asyncio.sleep(1)
+            self.battle_rooms.append(Room(room_id=room_obj.id))
+            await self.opening_words(room_obj)
+            await self.dt_opp_poke(room_obj)
 
     async def on_receive(self, room_id, inp_type, params):
         if inp_type in ['player']:
-            self.players[params[0]] = {'name': params[1], 'pokemon': []}
+            await self.add_player(room_id, params[0], params[1])
         elif inp_type in ['poke']:
-            name, gender = params[1].split(", ")
-            poke = {'name': name, 'type': [], 'gender': gender, 'moves': [], 'item': ''}
-            # self.players[params[0]].append(Pokemon(name=name, gender=gender))
-            self.players[params[0]]['pokemon'].append(poke)
+            species, gender = params[1].split(", ")
+            self.pokemon.append(Pokemon(room=room_id, player=params[0], species=species, types=[], moves=[], gender=gender, item='unknown'))
         elif inp_type in ['teampreview']:
             await self.rooms[room_id].start_poke(random.choice([1, 2, 3]))
         elif inp_type in ['request']:
-            await asyncio.sleep(1)
             if 'canMegaEvo' in params[0]:
-                self.mega = True
+                self.set_mega(room_id, True)
         elif inp_type in ['turn']:
             await asyncio.sleep(1)
-            await self.rooms[room_id].move(random.choice([1, 2, 3, 4]), True if self.mega else False)
-            if self.mega:
-                self.mega = False
+            mega = self.get_mega(room_id)
+            await self.rooms[room_id].move(random.choice([1, 2, 3, 4]), True if mega else False)
+            if mega:
+                self.set_mega(room_id, False)
         elif inp_type in ['win']:
-            if 'makbotdotpy' in params[0]:
-                await self.rooms[room_id].say("lol gg")
-            else:
-                await self.rooms[room_id].say("ggwp")
-            await asyncio.sleep(1)
-            await self.rooms[room_id].leave()
+            await self.closing_words(room_id, params[0])
         elif inp_type in ['raw']:
-            types = re.findall(r'(?<=_blank">)(\w+)|(?<=alt=")(\w+)', params[0])
-            if len(types) > 0:
-                for player in self.players:
-                    if self.players[player]['name'] != 'makbotdotpy':
-                        for i in range(len(self.players[player]['pokemon'])):
-                            if self.players[player]['pokemon'][i]['name'] in types[0]:
-                                for match in types[1:]:
-                                    self.players[player]['pokemon'][i]['type'].append(match[1])
-                                await self.rooms[room_id].say(self.players[player]['pokemon'][i])
+            await self.find_typing(room_id, params[0])
         elif inp_type in ['switch']:
-            await self.rooms[room_id].say("[debug] a pokemon has switched in")
-            
+            raw_player_id, nickname = params[0].split(': ')
+            raw_species = params[1]
+            self.set_nickname(raw_player_id, nickname, raw_species)
+        elif inp_type in ['move']:
+            raw_player_id, user = params[0].split(': ')
+            move = params[1]
+            # raw_target = params[2]
+            self.set_move(raw_player_id, user, move)
+    
+    def set_mega(self, room_id, value):
+        for i in range(len(self.battle_rooms)):
+            if self.battle_rooms[i].id == room_id:
+                self.battle_rooms[i].mega = value
+
+    def get_mega(self, room_id):
+        for room in self.battle_rooms:
+            if room.id == room_id:
+                return room.mega
+
+    def get_primary_player(self, room_id):
+        for room in self.battle_rooms:
+            if room.id == room_id:
+                return room.primary_player
+
+    def set_nickname(self, raw_player_id, nickname, raw_species):
+        for i in range(len(self.pokemon)):
+            if self.pokemon[i].player in raw_player_id and self.pokemon[i].species in raw_species:
+                self.pokemon[i].name = nickname
+
+    def set_move(self, raw_player_id, user, move):
+        for i in range(len(self.pokemon)):
+            if self.pokemon[i].player in raw_player_id and self.pokemon[i].name == user:
+                if move not in self.pokemon[i].moves:
+                    self.pokemon[i].moves.append(move)
+
+    async def add_player(self, room_id, player_id, player_name):
+        self.players.append(Player(room_id, player_id, player_name))
+        if player_name == self.name:
+                for i in range(len(self.battle_rooms)):
+                    if self.battle_rooms[i].id == room_id:
+                        self.battle_rooms[i].primary_player = player_id
+
+    async def opening_words(self, room_obj):
+        await room_obj.say('hi im the makbot')
+        await asyncio.sleep(1)
+        await room_obj.say("glhf")
+        await asyncio.sleep(1)
+
+    async def closing_words(self, room_id, raw_data):
+        if 'makbotdotpy' in raw_data:
+            await self.rooms[room_id].say("lol gg")
+        else:
+            await self.rooms[room_id].say("ggwp")
+        await asyncio.sleep(1)
+        await self.rooms[room_id].leave()
+    
+    async def dt_opp_poke(self, room_obj):
+        for poke in self.pokemon:
+            if poke.player != self.get_primary_player(room_obj.id):
+                await room_obj.say('!dt ' + poke.species)
+                await asyncio.sleep(1)
+    
+    async def find_typing(self, room_id, raw_data):
+        types = re.findall(r'(?<=_blank">)(\w+)|(?<=alt=")(\w+)', raw_data)
+        if len(types) > 0:
+            for i in range(len(self.pokemon)):
+                if self.pokemon[i].player != self.get_primary_player(room_id):
+                    if self.pokemon[i].species == types[0][0]:
+                        for match in types[1:]:
+                            self.pokemon[i].types.append(match[1])
 
 logging.basicConfig(level=logging.INFO)
 with open('./login/login.txt', 'rt') as f,\
@@ -93,4 +171,15 @@ with open('./login/login.txt', 'rt') as f,\
     team1v1 = team.read()
     username, password = f.read().strip().splitlines()
 
-ChallengeClient(name=username, password=password).start()
+client = ChallengeClient(name=username, password=password)
+client.start()
+
+print('BATTLE ROOMS')
+for room in client.battle_rooms:
+    print(room.print())
+print('PLAYERS')
+for player in client.players:
+    print(player.print())
+print('POKEMON')
+for poke in client.pokemon:
+    print(poke.print())
